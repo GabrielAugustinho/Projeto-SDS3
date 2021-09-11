af8ed71d2073f4430bcb762f20b142c360eba3ca
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-09-10 22:59:39 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -11,7 +11,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_sales")
+@Table(name = "tb_seles")
 public class Sale {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
